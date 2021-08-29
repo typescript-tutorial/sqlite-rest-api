ef437012e7d059fbaf8f8fd8c5a439c68612089d
Duc Nguyen <minhduc140583@gmail.com>
2021-08-29 22:47:07 +0700

    Refactor sqlite

--- src/services/sqlite/SqlUserService.ts
+++ src/services/sqlite/SqlUserService.ts
@@ -31,17 +31,15 @@ export class SqlUserService {
     return exec(this.db, `insert into users (userId, name, email) values ($1, $2, $3)`, [user.userId, user.name, user.email]);
   }
   insertMany(users: User[]): Promise<number> {
-    
-  const statements = users.map(stmt => {
-    return { query: `insert into users (userId, name, email) values ($1, $2, $3)`, params: [stmt.userId, stmt.name, stmt.email] };
-  });
-    
+    const statements = users.map(stmt => {
+      return { query: `insert into users (userId, name, email) values ($1, $2, $3)`, params: [stmt.userId, stmt.name, stmt.email] };
+    });
   // const s = `insert into users (userId, name, email) values (8, 'user8', 'user8@gmail.com');insert into users (userId, name, email) values (6, 'user8', 'user8@gmail.com');
   // insert into users (userId, name, email) values (9, 'user9', 'user9@gmail.com');`;
   // const st: Statement = {query: s};
   // const statements = [st];
   // return saveBatch(this.db, users, 'users', userModel.attributes);
-  return execBatch(this.db, statements, true);
+    return execBatch(this.db, statements, true);
   }
   update(user: User): Promise<number> {
     return exec(this.db, `update users set name=$1, email=$2 where userId = $3`, [user.name, user.email, user.userId]);
